1433ae6bd9c78f01cbb559450a741aa3767693f1
llmh3624 <llmh3624@gmail.com>
2023-04-24 14:10:02 +0200

    Added open-source code link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
-
+      <p>
+        <a href="https://github.com/llmh3624/weather-react-app" target="_blank">
+          Open-source code
+        </a>{" "}
+        by Laura Moragues
+      </p>
     </div>
   );
 }
